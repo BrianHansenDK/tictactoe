5dd8fbe0323682c214f40ff34ce59d13cde33613
brian <brianhansen.work@gmail.com>
2022-10-25 07:08:55 +0200

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App by Noaly!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <small>Hi from Brian haha</small>
+    </>
+  );
+};
+
+export default App;
