b236ed20f267e67ec93e9cba8deeb6c8f2edca32
brian <brianhansen.work@gmail.com>
2022-10-25 12:37:50 +0200

    Calculate for winner, who is up next and output on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
 import './styles/root.css';
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) {
+      return;
+    }
+
+    setBoard(previousSate => {
+      return previousSate.map((Square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+
+        return Square;
+      });
+    });
+
+    setIsXNext(previousSate => !previousSate);
+  };
   return (
     <div className="app">
       <h1>TIC TAC TOE</h1>
-      <Board />
+      <h2>{message}</h2>
+      <Board board={board} handleSquareClick={handleSquareClick} />
     </div>
   );
 };
